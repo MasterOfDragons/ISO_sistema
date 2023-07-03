82a3cdd1c1854793110f3c9be5248128cd220739
MasterOfDragons <darwinleosalmeron@gmail.com>
2023-07-03 17:26:23 -0600

    Edit footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2019 <a href="https://www.compartiendocodigos.net/" target="_blank">Compartiendo Códigos</a>.</strong>
+	<strong>Copyright &copy; 2023 <a href="Uni.edu.ni" target="_blank">Desarrolladores Uni</a>.</strong>
 
 	Todos los derechos reservados.
 
